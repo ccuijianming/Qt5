afea7f9f287262568cfcafb6099f1a280f590afa
ccuijianming <33944625+ccuijianming@users.noreply.github.com>
2020-05-29 16:29:11 +0800

    Add files via upload

---
+++ DemoQWidgetQStringListModel/widget.h
@@ -0,0 +1,41 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QStringListModel>
+
+#pragma execution_character_set("utf-8")
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private slots:
+    void on_btnListAppend_clicked();
+
+    void on_btnListInsert_clicked();
+
+    void on_btnListDelete_clicked();
+
+    void on_btnListClear_clicked();
+
+    void on_btnTextClear_clicked();
+
+    void on_btnListToText_clicked();
+
+    void on_listView_clicked(const QModelIndex &index);
+
+private:
+    Ui::Widget *ui;
+    QStringListModel *theModel;
+    QModelIndex index;
+};
+#endif // WIDGET_H
